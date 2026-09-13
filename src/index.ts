fe15605103ba453986e365fa4d0722e6f8be66e0
Jerry Du <jerrydu07@gmail.com>
2026-09-13 11:28:44 -0400

    feat(portfolio): added portfolio valuation metrics

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,19 @@ interface Position {
     shares: number,
     currentPrice: number,
     averagePurchasePrice: number,
-}
+};
 
 const portfolio: Position[] = [{ticker: 'AAPL', shares: 10, currentPrice: 230,
      averagePurchasePrice: 190}, {ticker: 'NVDA', shares: 5, currentPrice: 180, averagePurchasePrice: 140}, 
-    {ticker: 'GOOGL', shares: 8, currentPrice: 240, averagePurchasePrice: 200}]
+    {ticker: 'GOOGL', shares: 8, currentPrice: 240, averagePurchasePrice: 200}];
+
+function marketVal(position: Position) { 
+    return position.shares * position.currentPrice 
+};
+
+const marketValue = portfolio.map(marketVal);
+const totalVal = marketValue.reduce((sum , val) =>  sum + val, 0);
+
+console.log(marketValue) ;
+console.log(totalVal);
+
