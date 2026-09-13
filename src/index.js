d1a086bf61e1f06989f780c6088191850b8eefa7
Jerry Du <jerrydu07@gmail.com>
2026-09-13 10:51:23 -0400

    Added position interface and portfolio

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const portfolio = [{ ticker: 'AAPL', shares: 10, currentPrice: 230,
+        averagePurchasePrice: 190 }, { ticker: 'NVDA', shares: 5, currentPrice: 180, averagePurchasePrice: 140 },
+    { ticker: 'GOOGL', shares: 8, currentPrice: 240, averagePurchasePrice: 200 }];
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+interface Position { 
+    ticker: string,
+    shares: number,
+    currentPrice: number,
+    averagePurchasePrice: number,
+}
+
+const portfolio: Position[] = [{ticker: 'AAPL', shares: 10, currentPrice: 230,
+     averagePurchasePrice: 190}, {ticker: 'NVDA', shares: 5, currentPrice: 180, averagePurchasePrice: 140}, 
+    {ticker: 'GOOGL', shares: 8, currentPrice: 240, averagePurchasePrice: 200}]
